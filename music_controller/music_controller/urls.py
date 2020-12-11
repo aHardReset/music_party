fa29fffc003ba5f6a43fddb4f6f6fceb5d5d8b69
aHardReset <al14760427@ite.edu.mx>
2020-12-11 14:52:49 -0800

    After 3rd tutorial

--- music_controller/music_controller/urls.py
+++ music_controller/music_controller/urls.py
@@ -19,5 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('', include('frontend.urls'))
 ]
