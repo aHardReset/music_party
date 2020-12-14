320fa729361393c4669d0b8a1bfc798497eba4ec
aHardReset <al14760427@ite.edu.mx>
2020-12-13 20:53:56 -0800

    Adter 5th tutorial some fix

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -29,19 +29,21 @@ class CreateRoomView(APIView):
             self.request.session.create()
         
         serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            guest_can_pause = serializer.data.get('guest_can_pause')
-            votes_to_skip = serializer.data.get('votes_to_skip')
-            host = self.request.session.session_key
-            queryset = Room.objects.filter(host=host)
+        if not serializer.is_valid():
+            return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        guest_can_pause = serializer.data.get('guest_can_pause')
+        votes_to_skip = serializer.data.get('votes_to_skip')
+        host = self.request.session.session_key
+        queryset = Room.objects.filter(host=host)
 
-            if queryset.exists():
-                room = queryset[0]
-                room.guest_can_pause = guest_can_pause
-                room.votes_to_skip = votes_to_skip
-                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
-            else:
-                room = Room(host= host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
-                room.save()
-
+        if queryset.exists():
+            room = queryset[0]
+            room.guest_can_pause = guest_can_pause
+            room.votes_to_skip = votes_to_skip
+            room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+            return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+        else:
+            room = Room(host= host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
+            room.save()
             return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
