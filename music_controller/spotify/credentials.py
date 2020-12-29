d49a308cd6b83aa32bb627971c3415a0579ab197
aaroncontreras4 <al14760427@ite.edu.mx>
2020-12-29 00:22:48 -0800

    Documentation about the temp file credentials.py

--- music_controller/spotify/credentials.py
+++ music_controller/spotify/credentials.py
@@ -1,6 +1,20 @@
+'''
+This file is added and untracked to prevent share credentials
+and sensitive info.
+
+https://stackoverflow.com/questions/3319479/can-i-git-commit-a-file-and-ignore-its-content-changes
+
+git update-index --assume-unchanged [<file> ...]
+
+To undo and start tracking again:
+git update-index --no-assume-unchanged [<file> ...]
+
+Under dev. if more urls will be need to add be sure to hide secrets
+soon this file will be deleted and make this in enviroment variables and .ini files if needed
+'''
+
 AUTHORIZE_URL = "https://accounts.spotify.com/authorize"
 TOKEN_URL = "https://accounts.spotify.com/api/token"
 REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect",
-BASE_URL = "https://api.spotify.com/v1/me/"
 CLIENT_ID = ""
 CLIENT_SECRET = ""
