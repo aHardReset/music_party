7357060c05e18f88545a2334c73ef8b387acd824
aaroncontreras4 <al14760427@ite.edu.mx>
2020-12-29 00:10:42 -0800

    Add credentials.py for homogeneity in clones

---
+++ music_controller/spotify/credentials.py
@@ -0,0 +1,6 @@
+AUTHORIZE_URL = "https://accounts.spotify.com/authorize"
+TOKEN_URL = "https://accounts.spotify.com/api/token"
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect",
+BASE_URL = "https://api.spotify.com/v1/me/"
+CLIENT_ID = ""
+CLIENT_SECRET = ""
